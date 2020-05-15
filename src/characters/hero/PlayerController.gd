e9a10a90b671cdf01cad767c0db1fac30eeb0465
Jan <jan.feldhorst@gmail.com>
2020-05-15 10:16:21 +0200

    New character sprites and animations

--- src/characters/hero/PlayerController.gd
+++ src/characters/hero/PlayerController.gd
@@ -8,10 +8,10 @@ var can_move : bool = true
 var equipped_tool : Tool
 
 onready var directions : Dictionary = {
-	"up" : Vector2(0, -1),
-	"down" : Vector2(0, 1),
-	"right" : Vector2(1, 0),
-	"left" : Vector2(-1, 0)
+	"up" : Vector2.UP,
+	"down" : Vector2.DOWN,
+	"right" : Vector2.RIGHT,
+	"left" : Vector2.LEFT
 }
 
 var look_dir : Vector2
@@ -23,26 +23,29 @@ func _ready():
 
 func _physics_process(_delta):
 	var _collider : KinematicCollision2D
+	var is_moving : bool = false
 
 	var prev_dir = look_dir
 
 	if Input.is_action_pressed("ui_up"):
 		look_dir = directions["up"]
 		velocity.y = -speed
+		is_moving = true
 	elif Input.is_action_pressed("ui_down"):
 		look_dir = directions["down"]
 		velocity.y = speed
+		is_moving = true
 	else:
 		velocity.y = 0
 
 	if Input.is_action_pressed("ui_right"):
 		look_dir = directions["right"]
 		velocity.x = speed
-		$AnimatedSprite.flip_h = false
+		is_moving = true
 	elif Input.is_action_pressed("ui_left"):
 		look_dir = directions["left"]
 		velocity.x = -speed
-		$AnimatedSprite.flip_h = true
+		is_moving = true
 	else:
 		velocity.x = 0
 
@@ -57,12 +60,14 @@ func _physics_process(_delta):
 	if can_move:
 		velocity = move_and_slide(velocity)
 
+	play_animation(is_moving, look_dir)
+
 
 func get_equipped_tool() -> void:
 	equipped_tool = $Tool.get_child(0)
 
 
-func flip_tool(direction : Vector2):
+func flip_tool(direction : Vector2) -> void:
 	var tool_offset = 8
 
 	$Tool.position = direction * tool_offset
@@ -71,3 +76,27 @@ func flip_tool(direction : Vector2):
 		equipped_tool.get_node("AnimatedSprite").flip_h = true
 	else:
 		equipped_tool.get_node("AnimatedSprite").flip_h = false
+
+
+func play_animation(is_moving : bool, direction : Vector2) -> void:
+	match direction:
+		Vector2.UP:
+			if is_moving:
+				$AnimatedSprite.play("walk_up")
+			else:
+				$AnimatedSprite.play("idle_up")
+		Vector2.DOWN:
+			if is_moving:
+				$AnimatedSprite.play("walk_down")
+			else:
+				$AnimatedSprite.play("idle_down")
+		Vector2.LEFT:
+			if is_moving:
+				$AnimatedSprite.play("walk_left")
+			else:
+				$AnimatedSprite.play("idle_left")
+		Vector2.RIGHT:
+			if is_moving:
+				$AnimatedSprite.play("walk_right")
+			else:
+				$AnimatedSprite.play("idle_right")
--- src/environment/tree/Repository.gd
+++ src/environment/tree/Repository.gd
@@ -6,20 +6,23 @@ export (PackedScene) var ressource
 
 var health : int = 100
 
+var is_depleted : bool = false
+
 
 func _ready():
 	$AnimatedSprite.frame = 0
 
 
-
 func take_damage(damage : int) -> void:
 	health -= damage
 
-	if health <= 0:
+	if health <= 0 and !is_depleted:
 		die()		
 
 
 func die():
+	is_depleted = true
+
 	$AnimatedSprite.frame = 1
 
 	var drop = ressource.instance()
